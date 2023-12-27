ccf999585ac0091ec6c3c96426491f96ec6a9cab
Marcelo Alves <alves-marcelo97@hotmail.com>
2023-12-26 23:54:53 -0300

    feat:(profile, authenticate) jwt

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,12 @@
-import { FastifyInstance } from 'fastify'
 import { authenticate } from '@/http/controllers/authenticate'
-import { register } from './controllers/register'
+import { profile } from '@/http/controllers/profile'
+import { register } from '@/http/controllers/register'
+import { FastifyInstance } from 'fastify'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
+
+  // Authenticated
+  app.get('/me', profile)
 }
